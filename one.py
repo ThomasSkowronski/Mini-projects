c256ff4c20aaf767dcd8f4a8a0d52670d1dae011
Thomas <skowrontom@gmail.com>
2021-09-24 23:50:28 -0400

    new

--- one.py
+++ one.py
@@ -1,1 +1,32 @@
-print ('hello')
+def app():
+    print ('What would you like to do?')
+
+    ins = input('''
+0: exit
+1: calculate the seconds in a number of days
+2: reverse a string
+
+''')
+    if ins=='1':
+        secondsDays()
+    elif ins=='0':
+        return
+    elif ins=='2':
+        strReverse()
+    else:
+        app()
+
+def secondsDays():
+    days = input('how many days? ')
+    seconds = int(days)*24*60*60
+
+    print (str(seconds)+(' seconds in ')+days+(' days.'))
+    app()
+
+def strReverse():
+    st=input ('string to reverse: ')
+    out=st[::-1]
+    print (out)
+    app()
+
+app()
---
+++ tictacto.py
@@ -0,0 +1,60 @@
+import tkinter as tk
+
+xTurn = True
+btns = [0,0,0,0,0,0,0,0,0]
+
+def btnEnter(event):
+    if (btns[btnPos(event)]==0):
+        if (xTurn):
+            event.widget.configure(text='X')
+        else:
+            event.widget.configure(text='O')
+
+def btnLeave(event):
+    if (btns[btnPos(event)]==0):
+        event.widget.configure(text='')
+
+def btnClick(event):
+    if (btns[btnPos(event)]==0):
+        global xTurn
+        if (xTurn):
+            btns[btnPos(event)] = 1
+        else:
+            btns[btnPos(event)] = 2
+        xTurn = not xTurn
+
+def btnPos(event):
+    row = event.widget.grid_info()['row']
+    col = event.widget.grid_info()['column']
+    index = (row*3)+col
+    return index 
+
+window = tk.Tk()
+window.title('Tic Tac Toe')
+
+window.columnconfigure(0, weight=1, minsize=300)
+window.rowconfigure(0, weight=0, minsize=75)
+window.rowconfigure(1, weight=1, minsize=300)
+
+controlFrame = tk.Frame(window, background='white')
+controlFrame.columnconfigure(0, weight=1, minsize=80)
+controlFrame.rowconfigure(0, weight=1, minsize=80)
+controlFrame.grid(row=0, column=0, sticky='new')
+
+label = tk.Label(controlFrame, text='label', relief=tk.RAISED)
+label.grid(row=0, column=0, sticky='nesw')
+
+gameFrame = tk.Frame(window, background='white')
+gameFrame.grid(row=1, column=0, sticky='nesw')
+
+for i in range(3):
+    gameFrame.columnconfigure(i, weight=1, minsize=50)
+    gameFrame.rowconfigure(i, weight=1, minsize=50)
+    for j in range(3):
+        button = tk.Button(gameFrame, relief=tk.RAISED)
+        button.grid(row=i, column=j, sticky='nesw')
+        button.bind("<Enter>", btnEnter)
+        button.bind("<Leave>", btnLeave)
+        button.bind("<Button-1>", btnClick)
+
+window.mainloop()
