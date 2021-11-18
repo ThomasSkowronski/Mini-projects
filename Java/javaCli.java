25176d7bdb228b3e5d55f00f84e358ef435de2c8
Thomas <skowrontom@gmail.com>
2021-11-18 10:50:01 -0500

    Added java folder

---
+++ Java/javaCli.java
@@ -0,0 +1,10 @@
+public class javaCli {
+  public static void main(String[] args) {
+    System.out.println("Hello there");
+    printHi();
+  }
+
+  public static void printHi (){
+    System.out.println("mehod activated");
+  }
+}
